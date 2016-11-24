9143416db45f13c003b7bb90f4b99c1511b70811
David S. Batista <dsbatista@gmail.com>
2016-11-24 15:49:52 +0000

    updated package name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='surveygizmo-hellofresh',
+    name='surveygizmoHF',
     version=__version__,
     description='A Python Wrapper for SurveyGizmo\'s restful API service.',
     long_description=README,
